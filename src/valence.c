0dfc7f9b376d09defd44d2d009155eb552013902
xts-sec <ubuntu@ip-172-31-40-181.eu-west-2.compute.internal>
2023-09-18 14:24:42 +0000

    fixed segfault on empty command

--- src/valence.c
+++ src/valence.c
@@ -8,7 +8,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
 //
 // Define Colour Profiles
 // Uncomment as used to keep compiled size down
@@ -30,59 +29,49 @@
 //#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
 //#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
 //#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
-
 // define token delimiters
 #define TOKEN_DELIM " \t\r\n"
 #define DEFAULT_BUFFER_SIZE 1024
-
 // declarations for backend functions
-char *read_line();
-char **split_line(char *);
+char  * read_line();
+char **split_line(char  * );
 int valence_execute(char **);
-
 // declarations for builtin shell functions
 int valence_exit();
 int valence_cd(char **args);
 int valence_help();
-
-
 //  List of builtin command names
-char *builtin_names[] = {
-	"help 	- display this help info",
-	"cd 	- change current working directory",
-	"exit 	- kill this shell"
+char  * builtin_names[] = {
+    "help 	- display this help info", "cd 	- change current working directory", "exit 	- kill this shell"
 };
-
 // list of builtin command functions
-int (*builtin_functions[]) (char **) = {
-  &valence_help,
-  &valence_cd,
-  &valence_exit
+int ( * builtin_functions[]) (char **) = {
+    &valence_help, &valence_cd, &valence_exit
 };
-
 // calculate the number of builtin command names
 int valence_num_builtins() {
-  return sizeof(builtin_names) / sizeof(char *);
+    return sizeof(builtin_names)  /  sizeof(char  * );
 }
 
 //
 // name: valence_help
 // desc: display halp info to user, including built in command list
 // args: none
-int valence_help(){
-	fprintf(stdout, GREEN "#################################################\n");
-	fprintf(stdout, "# valence shell - made by xts-sec               #\n");
-	fprintf(stdout, "# PRE_RELEASE VERSION - WORK IN PROGRESS        #\n");
-	fprintf(stdout, "#################################################\n" RESET);
-	fprintf(stdout, "Built in commands:\n");
-	for(int i = 0; i < valence_num_builtins(); i++) {
-    	printf("  %s\n", builtin_names[i]);
-  	}
-  	printf("Use the man command for information on other programs.\n");
-  	return 1;
+int valence_help() {
+    fprintf(stdout, GREEN "#################################################\n");
+    fprintf(stdout, "# valence shell - made by xts-sec               #\n");
+    fprintf(stdout, "# PRE_RELEASE VERSION - WORK IN PROGRESS        #\n");
+    fprintf(stdout, "#################################################\n" RESET);
+    fprintf(stdout, "Built in commands:\n");
+    for(int i = 0;
+    i < valence_num_builtins();
+    i++) {
+        printf("  %s\n", builtin_names[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
 }
-
-
 
 //
 // name: valence_exit
@@ -90,28 +79,27 @@ int valence_help(){
 // args: character array "args"
 //
 int valence_exit() {
-  return 0;
+    return 0;
 }
 
 //
 // name: valence_cd
 // desc: changes the current working directory
 // args: character array "args"
-int valence_cd(char **args){
-	
-	if (args[1] == NULL) {
-		//fprintf(stderr, RED "valence: argument expected\n" RESET);
-		fprintf(stderr, GREEN "valence: cd: no argument provided, going home\n" RESET);
-		chdir(getenv("HOME"));
-	 } else {
-	 	if (chdir(args[1]) != 0) {
-			fprintf(stderr, RED "valence: cd: invalid driectory\n" RESET);
-	    }
-	}
-	return 1;
+int valence_cd(char **args) {
+    if (args[1] == NULL)  {
+        //fprintf(stderr, RED "valence: argument expected\n" RESET);
+        fprintf(stderr, GREEN "valence: cd: no argument provided, going home\n" RESET);
+        chdir(getenv("HOME"));
+    } else {
+        if (chdir(args[1]) != 0)  {
+            fprintf(stderr, RED "valence: cd: invalid driectory\n" RESET);
+        }
+
+    }
+
+    return 1;
 }
-
-
 
 //
 // name: valence_execute
@@ -119,118 +107,117 @@ int valence_cd(char **args){
 // args: character array "args"
 //
 int valence_execute(char **args) {
+    // 
+    pid_t cpid;
+    int status;
+    if (args[0] == NULL)  {
+        return 1;
+    } else if (strcmp(args[0], "exit") == 0)  {
+        return valence_exit();
+    } else if (strcmp(args[0], "cd") == 0)  {
+        return valence_cd(args);
+    } else if (strcmp(args[0], "help") == 0)  {
+        return valence_help();
+    }
 
-	// 
-  	pid_t cpid;
-  	int status;
+    cpid = fork();
+    if (cpid == 0)  {
+        if (execvp(args[0], args) < 0) 
+            printf(RED "valence: command not found: %s\n" RESET, args[0]);
+        exit(EXIT_FAILURE);
+    } else if (cpid < 0) 
+        printf(RED "Error forking" RESET "\n");
+    else {
+        waitpid(cpid, & status, WUNTRACED);
+    }
 
-  if (strcmp(args[0], "exit") == 0) {
-    return valence_exit();
-  } else if (strcmp(args[0], "cd") == 0){
-  	return valence_cd(args);
-  } else if (strcmp(args[0], "help") == 0){
-  	return valence_help();
-  }
-
-  cpid = fork();
-
-  if (cpid == 0) {
-    if (execvp(args[0], args) < 0)
-      printf(RED "valence: command not found: %s\n" RESET, args[0]);
-    exit(EXIT_FAILURE);
-
-  } else if (cpid < 0)
-    printf(RED "Error forking" RESET "\n");
-  else {
-    waitpid(cpid, & status, WUNTRACED);
-  }
-  return 1;
+    return 1;
 }
 
-
-
-char **split_line(char * line) {
-  int buffsize = DEFAULT_BUFFER_SIZE;
-  int position = 0;
-  char **tokens = malloc(buffsize * sizeof(char *));
-  char *token;
-
-  if (!tokens) {
-    fprintf(stderr, RED "valence: Allocation error\n" RESET);
-    exit(EXIT_FAILURE);
-  }
-  token = strtok(line, TOKEN_DELIM);
-  while (token != NULL) {
-    tokens[position] = token;
-    position++;
-
-    if (position >= buffsize) {
-      buffsize += DEFAULT_BUFFER_SIZE;
-      tokens = realloc(tokens, buffsize * sizeof(char * ));
-
-      if (!tokens) {
+char **split_line(char  *  line) {
+    int buffsize = DEFAULT_BUFFER_SIZE;
+    int position = 0;
+    char **tokens = malloc(buffsize  *  sizeof(char  * ));
+    char  * token;
+    if (!tokens)  {
         fprintf(stderr, RED "valence: Allocation error\n" RESET);
         exit(EXIT_FAILURE);
-      }
     }
-    token = strtok(NULL, TOKEN_DELIM);
-  }
-  tokens[position] = NULL;
 
-  return tokens;
+    token = strtok(line, TOKEN_DELIM);
+    while (token != NULL)  {
+        tokens[position] = token;
+        position++;
+        if (position >= buffsize)  {
+            buffsize += DEFAULT_BUFFER_SIZE;
+            tokens = realloc(tokens, buffsize  *  sizeof(char  *  ));
+            if (!tokens)  {
+                fprintf(stderr, RED "valence: Allocation error\n" RESET);
+                exit(EXIT_FAILURE);
+            }
+
+        }
+
+        token = strtok(NULL, TOKEN_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
 }
 
-char *read_line() {
-  int buffsize = DEFAULT_BUFFER_SIZE;
-  int position = 0;
-  char *buffer = malloc(sizeof(char) * buffsize);
-  int c;
-
-  if (!buffer) {
-    fprintf(stderr, RED "dash: Allocation error\n");
-    exit(EXIT_FAILURE);
-  }
-
-  while (1) {
-    c = getchar();
-    if (c == '\r' || c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
-
-    if (position >= buffsize) {
-      buffsize += DEFAULT_BUFFER_SIZE;
-      buffer = realloc(buffer, buffsize);
-
-      if (!buffer) {
-        fprintf(stderr, RED "valence: Allocation error\n" RESET);
+char  * read_line() {
+    int buffsize = DEFAULT_BUFFER_SIZE;
+    int position = 0;
+    char  * buffer = malloc(sizeof(char)  *  buffsize);
+    int c;
+    if (!buffer)  {
+        fprintf(stderr, RED "dash: Allocation error\n");
         exit(EXIT_FAILURE);
-      }
     }
-  }
+
+    while (1)  {
+        c = getchar();
+        if (c == EOF || c == '\n' || c == '\r' || c == EOF)  {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+
+        position++;
+        if (position >= buffsize)  {
+            buffsize += DEFAULT_BUFFER_SIZE;
+            buffer = realloc(buffer, buffsize);
+            if (!buffer)  {
+                fprintf(stderr, RED "valence: Allocation error\n" RESET);
+                exit(EXIT_FAILURE);
+            }
+
+        }
+
+    }
+
 }
 
 void loop() {
-	char *line;
-	char **args;
-	int status = 1;
-	char *cwd = malloc(sizeof(char) *  DEFAULT_BUFFER_SIZE);
+    char  * line;
+    char **args;
+    int status = 1;
+    char  * cwd = malloc(sizeof(char)  *   DEFAULT_BUFFER_SIZE);
+    do {
+        getcwd(cwd, DEFAULT_BUFFER_SIZE);
+        printf("valence: " BLUE "%s " RESET  "» ", cwd);
+        line = read_line();
+        args = split_line(line);
+        status = valence_execute(args);
+        free(line);
+        free(args);
+    }
 
-	do {
-		getcwd(cwd, DEFAULT_BUFFER_SIZE);
-		printf("valence: " BLUE "%s " RESET  "» ", cwd);
-    	line = read_line();
-    	args = split_line(line);
-    	status = valence_execute(args);
-    	free(line);
-    	free(args);
-  	} while (status);
+    while (status);
 }
 
 int main() {
-  loop();
-  return 0;
+    loop();
+    return 0;
 }
